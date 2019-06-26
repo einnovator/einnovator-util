7768fff30245726b587788ce562ef596114ef231
Jorge Simao <jsimao71@gmail.com>
2019-06-26 22:57:41 +0100

    remove method from TextTemplates

--- src/main/java/org/einnovator/util/TextTemplates.java
+++ src/main/java/org/einnovator/util/TextTemplates.java
@@ -1,14 +1,6 @@
 package org.einnovator.util;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.StringWriter;
-import java.nio.charset.Charset;
 import java.util.Map;
-
-import org.apache.commons.io.IOUtils;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 
 public class TextTemplates {
 
@@ -93,18 +85,5 @@ public class TextTemplates {
 	}
 	
 	
-	
-	public static String readResource(String filename) {
-		try {
-			Resource resource = new ClassPathResource(filename);
-			InputStream inputStream = resource.getInputStream();
-			StringWriter writer = new StringWriter();
-			IOUtils.copy(inputStream, writer, Charset.forName("UTF-8"));
-			String s = writer.toString();
-			return s;
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-	}
 	
 }
